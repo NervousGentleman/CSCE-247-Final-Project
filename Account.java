bed93efb08989af429758517d83f13806baabcd4
NervousGentlman <ecoarr@email.sc.edu>
2022-03-12 23:03:31 -0500

    Laying the Groundwork

---
+++ Account.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class Account {
+
+
+    private String email;
+    private String userPassword;
+    private ArrayList<Flight> flightHistory;
+    private ArrayList<Hotel> hotelHistory;
+    private Boolean frequentFlyer;
+    private ArrayList<Flight> bookedFlights;
+    private Boolean smoker;
+    private ArrayList<Passeneger> guests;
+
+    public String getEmail()
+    {
+
+    }
+    public void setEmail(String email)
+    {
+
+    }
+    public ArrayList<Flight> getFlightHistory()
+    {
+
+    }
+    public void addFlightHistory(Flight flight)
+    {
+
+    }
+    public
+    
+}
