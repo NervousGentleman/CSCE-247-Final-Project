8d4ea28859344ba63ac07c748f9d1ae344e8ebfc
NervousGentlman <ecoarr@email.sc.edu>
2022-03-17 14:08:52 -0400

    messing around with stuff

--- Airline.java
+++ Airline.java
@@ -1,37 +1,41 @@
+import java.util.Scanner;
 public class Airline {
 
-    private int carryOnLImit;
+    private int carryOnLimit;
     private int checkedBagLimit;
     private String name;
     private int rating;
+    private String airlineName;
+
+    Scanner input = new Scanner(System.in);
 
     public int getCarryOnlimit()
     {
-
+        return carryOnLimit;
     }
     public void setCarryOnLimit(int carryOnLimit)
     {
-
+        carryOnLimit == input.PLACEHOLDER;
     }
     public int getCheckedBagLimit()
     {
-
+        return checkedBagLimit;
     }
     public void setCheckedBagLimit(int checkedBagLimit)
     {
-
+        checkedBagLimit == input.PLACEHOLDER;
     }
     public String getAirlineName()
     {
-
+        return airlineName;
     }
     public void setAirlineName(String airlineName)
     {
-
+        airlineName == input.PLACEHOLDER;
     }
     public int getRating()
     {
-
+        return rating;
     }
     public void setRaiting(int rating)
     {
