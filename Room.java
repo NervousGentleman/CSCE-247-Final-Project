e061986dfc6ee9d5c16334c34c38c4f4d9bde2de
NervousGentlman <ecoarr@email.sc.edu>
2022-03-17 13:37:09 -0400

    Laying the groundwork

--- Room.java
+++ Room.java
@@ -1,3 +1,33 @@
 public class Room {
+
+    private String roomtype;
+    private Boolean Smoker_Friendly;
+    private int bedCount;
+    private String bedType;
+
+    public void setRoomType(String roomtype)
+    {
+
+    }
+    public Boolean isSmoker_Frienldy()
+    {
+
+    }
+    public void setbedType(String betType)
+    {
+
+    }
+    public String getRoomType()
+    {
+
+    }
+    public int getbedCount()
+    {
+
+    }
+    public Stirng getbedType()
+    {
+
+    }
     
 }
