8c34552d2eb44c7920af96cd297020e19465aa1e
NervousGentlman <ecoarr@email.sc.edu>
2022-03-17 13:24:03 -0400

    Laying the Groundwork

--- Hotel.java
+++ Hotel.java
@@ -8,5 +8,54 @@ public class Hotel {
     private Boolean breakfast;
     private String hotel_address;
 
+    public String getName();
+    {
+
+    }
+    public void setName(String name)
+    {
+
+    }
+    public Double getPrice()
+    {
+
+    }
+    public void setPrice(Double price)
+    {
+
+    }
+    public Room getRoom_Type()
+    {
+
+    }
+    public void setRoom_TYpe(Room room_type)
+    {
+
+    }
+    public int getRating()
+    {
+
+    }
+    public void setRating(int rating)
+    {
+
+    }
+    public Boolean isPet_Friendly()
+    {
+
+    }
+    public Boolean hasBreakfast()
+    {
+
+    }
+    public String getHotelAddress()
+    {
+
+    }
+    public void setHotelAddress(String hotelAddress)
+    {
+
+    }
+
     
 }
