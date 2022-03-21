5c2a1f3a344bb42e823d1f3876d75e2f8492c32d
Cmeador18 <meadorbc@email.sc.edu>
2022-03-21 06:49:29 -0400

    adding list classes and Hotel Json

---
+++ PassengerList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class PassengerList {
+    
+    private ArrayList<Passenger> passengers;
+
+    public PassengerList(){
+
+
+    } // ending bracket of deault constructor
+
+    public void addPassenger(Passenger newPassenger){
+
+
+    } // ending bracket of method addPassenger
+
+    public void removePassenger(String passengerName){
+
+
+    } // ending bracket of method removePassenger
+
+    public void displayPassengers(){
+
+
+    } // ending bracket of method displayPassengers
+
+} // ending bracket of class PassengerList
