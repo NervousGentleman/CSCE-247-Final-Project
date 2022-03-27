bd9ea4ba1bb0fb24cee2a4d543894bf1e6db5b7e
murraypm <murraypm@email.sc.edu>
2022-03-27 03:17:52 -0400

    Added constructor to Passenger

--- Passenger.java
+++ Passenger.java
@@ -1,41 +1,43 @@
+import java.util.Date;
+
 public class Passenger {
-
     private String name;
-    private String dateOfBirth;
-    private String passportNumber;
-    private Seat seat;
+    private Date dateOfBirth;
+    private int passportNumber;
 
+    public Passenger(String name, Date dateOfBirth, int passportNumber) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.passportNumber = passportNumber;
+    }
+    
     public String getName()
     {
         return name;
     }
+
     public void setName(String name)
     {
         this.name = name;
     }
-    public String getDateOfBirth()
+
+    public Date getDateOfBirth()
     {
         return dateOfBirth;
     }
-    public void setDateOfBirth(String dateOfBirth)
+
+    public void setDateOfBirth(Date dateOfBirth)
     {
         this.dateOfBirth = dateOfBirth;
     }
-    public String getPassportNumber()
+
+    public int getPassportNumber()
     {
         return passportNumber;
     }
-    public void setPassportNumber(String passportNumber)
+
+    public void setPassportNumber(int passportNumber)
     {
         this.passportNumber = passportNumber;
     }
-    public Seat getSeat()
-    {
-        return seat;
-    }
-    public void setSeat(Seat seat)
-    {
-        this.seat = seat;
-    }
-    
 }
