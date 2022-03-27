92f63892cae0f83f1fc5024a2eb132b2b83b5ff5
murraypm <murraypm@email.sc.edu>
2022-03-27 01:55:01 -0400

    Changed Sort to interface

--- Sort.java
+++ Sort.java
@@ -1,37 +1,6 @@
-public class Sort {
+import java.util.ArrayList;
 
-    private String hotelName;
-    private String flightName;
-    private int flightPrice;
-    private int hotelPrice;
-    private int departureTime;
-    private int arrivalTime;
-
-
-    public void SorthotelName()
-    {
-        
-    }
-    public void SortflightName()
-    {
-
-    }
-    public void SortflightPrice()
-    {
-
-    }
-    public void SorthotelPrice()
-    {
-
-    }
-    public void SortdepartureTime()
-    {
-
-    }
-    public void SortarrivalTime()
-    {
-        
-    }
-
-    
+public interface Sort<T> {
+    public ArrayList<T> sortName();
+    public ArrayList<T> sortPrice();
 }
