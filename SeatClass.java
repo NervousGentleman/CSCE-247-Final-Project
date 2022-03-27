8cbb3629dcc24b93297890e5f24f56462a1a94f9
murraypm <murraypm@email.sc.edu>
2022-03-27 01:48:01 -0400

    Added getName function to SeatClass

--- SeatClass.java
+++ SeatClass.java
@@ -1,5 +1,15 @@
 public enum SeatClass{
+    ECONOMY("Economy"),
+    BUSINESS("Business"),
+    FIRST_CLASS("First Class");
 
-    FIRST,BUISNESS,ECONOMY
+    private final String name;
+    
+    private SeatClass(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
 }
