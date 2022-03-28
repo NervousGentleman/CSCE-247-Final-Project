92c0203795f174b36a9f1a0d2619c02580b290d3
Max Van Lokeren <mrv@email.sc.edu>
2022-03-28 14:27:08 -0400

    made some changes to sort

---
+++ HotelSort.java
@@ -0,0 +1,3 @@
+public class HotelSort implements Sort {
+    
+}
--- Sort.java
+++ Sort.java
@@ -1,37 +1,4 @@
-public class Sort {
-
-    private String hotelName;
-    private String flightName;
-    private int flightPrice;
-    private int hotelPrice;
-    private int departureTime;
-    private int arrivalTime;
-
-
-    public void SorthotelName()
-    {
-        
-    }
-    public void SortflightName()
-    {
-
-    }
-    public void SortflightPrice()
-    {
-
-    }
-    public void SorthotelPrice()
-    {
-
-    }
-    public void SortdepartureTime()
-    {
-
-    }
-    public void SortarrivalTime()
-    {
-        
-    }
-
-    
+public interface Sort<T> {
+    public ArrayList<T> sortName();
+    public ArrayList<T> sortPrice();
 }
