da38e85cd21e3476c30e986452db0f55847bc894
murraypm <murraypm@email.sc.edu>
2022-03-28 09:19:31 -0400

    Added FlightParser functionality

--- FlightJSONConstants.java
+++ FlightJSONConstants.java
@@ -1,5 +1,27 @@
 public enum FlightJSONConstants {
-    TEST("test");
+    FILE_NAME("JSON/flight.json"),
+    FLIGHT_ID("flightID"),
+    AIRLINE("airline"),
+    DEPARTURE_LOCATION("departureLocation"),
+    DESTINATION_LOCATION("destinationLocation"),
+    IS_CONNECTING("isConnecting"),
+    PREVIOUS_FLIGHT("previousFlight"),
+    NEXT_FLIGHT("nextFlight"),
+    DEPARTURE_TIME("departureTime"),
+    ARRIVAL_TIME("arrivalTime"),
+    FLIGHT_DURATION("flightDuration"),
+    NUM_ROWS("numRows"),
+    NUM_SEATS_PER_ROW("numSeatsPerRow"),
+    SEATS("seats"),
+    PASSENGER("passenger"),
+    NAME("name"),
+    DATE_OF_BIRTH("dateOfBirth"),
+    PASSPORT_NUMBER("passportNumber"),
+    COST("cost"),
+    CLASS("class"),
+    SEAT_ROW("seatRow"),
+    SEAT_LETTER("seatLetter"),
+    SEAT_TAKEN("seatTaken");
     
     private final String name;
 
