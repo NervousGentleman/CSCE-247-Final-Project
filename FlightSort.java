eb4e492f4fad615eb33986f1df786cfc0ae5e643
Max Van Lokeren <mrv@email.sc.edu>
2022-03-29 14:28:03 -0400

    changed the flight sort

--- FlightSort.java
+++ FlightSort.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class FlightSort {
     private ArrayList<Flight> flights;
+    private ArrayList<Seat> seats;
 
-    public FlightSort(ArrayList<Flight> flights) {
+    public FlightSort(ArrayList<Flight> flights, ArrayList<Seat> seats) {
         this.flights = flights;
+        this.seats = seats;
     }
 
-    public ArrayList<Flight> sortName() {
-        ArrayList<Flight> ret = flights;
-        return ret;
-    }
+    public Comparator<Flight> sortName = new Comparator<Flight>() {
+        public int compare(Flight f1, Flight f2){
+            String flight1 = f1.getAirline().toString().toUpperCase();
+            String flight2 = f2.getAirline().toString().toUpperCase();
+            return flight1.compareTo(flight2);
+        }
+    }; 
 
     public ArrayList<Flight> sortPrice() {
         ArrayList<Flight> ret = flights;
@@ -26,3 +32,4 @@ public class FlightSort {
         ArrayList<Flight> ret = flights;
         return ret;
     }
+}
