a72c08b0db46fbeb8baccffa8bfeb5d729eae5f8
Max Van Lokeren <mrv@email.sc.edu>
2022-03-30 18:11:19 -0400

    sorting the hotelsort

--- HotelSort.java
+++ HotelSort.java
@@ -1,19 +1,40 @@
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class HotelSort implements Sort<Hotel> {
+public class HotelSort {
     private ArrayList<Hotel> hotels;
+    private ArrayList<Room> rooms;
 
-    public HotelSort(ArrayList<Hotel> hotels) {
+    public HotelSort(ArrayList<Hotel> hotels, ArrayList<Room> rooms) {
         this.hotels = hotels;
+        this.rooms = rooms;
     }
 
-    public ArrayList<Hotel> sortName() {
-        ArrayList<Hotel> ret = hotels;
-        return ret;
+    public Comparator<Hotel> sortName = new Comparator<Hotel>() {
+        public int compare(Hotel h1, Hotel h2){
+            String hotel1 = h1.getName().toString().toUpperCase();
+            String hotel2 = h2.getName().toString().toUpperCase();
+            return hotel1.compareTo(hotel2);
+        }
+    }; 
+
+    public Comparator<Room> sortPrice = new Comparator<Room>() {
+        public int compare(Room s1, Room s2){
+            int room1 = (int)s1.getPrice();
+            int room2 = (int)s2.getPrice();
+            return room1 - room2;
+        }
+    };
+
+    public ArrayList<Hotel> sortNames(){
+        Collections.sort(hotels, sortName);
+        return hotels;
     }
 
-    public ArrayList<Hotel> sortPrice() {
-        ArrayList<Hotel> ret = hotels;
-        return ret;
+    public ArrayList<Room> sortPrices(){
+        Collections.sort(rooms, sortPrice);
+        return rooms;
     }
 }
